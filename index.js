1510522ab733edc452fc9caaab405754ea02beb5
kernoeb <kernoeb@protonmail.com>
2020-12-25 22:48:12 +0100

    add: capital mode, update json (remove some countries)

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const removeAccents = require('remove-accents')
 
 const keys = Object.keys(flags)
 const keysLength = keys.length
+
+const keysNoCapital = []
+for (const i of keys) {
+  if (flags[i]['capital']) keysNoCapital.push(i)
+}
+const keysNoCapitalLength = keysNoCapital.length
+
 const channels = {}
 
 const client = new Discord.Client()
@@ -21,14 +28,14 @@ client.on('ready', () => {
  * @param msg current message object
  * @param loop mode
  */
-async function sendQuestion(msg, loop) {
+async function sendFlag(msg, loop) {
   const randFlag = keys[Math.floor(Math.random() * keysLength)]
 
   if (channels[msg.channel.id]) {
     channels[msg.channel.id].flag = randFlag
     channels[msg.channel.id].pending = true
   } else {
-    channels[msg.channel.id] = {flag: randFlag, loop, pending: true}
+    channels[msg.channel.id] = {flag: randFlag, loop, mode: 'flag', pending: true}
   }
 
   await msg.channel.send(randFlag)
@@ -36,13 +43,48 @@ async function sendQuestion(msg, loop) {
 }
 
 /**
+ * Send the capital
+ * @param msg current message object
+ * @param loop mode
+ */
+async function sendCapital(msg, loop) {
+  const randFlag = keysNoCapital[Math.floor(Math.random() * keysNoCapitalLength)]
+
+  if (channels[msg.channel.id]) {
+    channels[msg.channel.id].flag = randFlag
+    channels[msg.channel.id].pending = true
+  } else {
+    channels[msg.channel.id] = {flag: randFlag, loop, mode: 'capital', pending: true}
+  }
+
+  await msg.channel.send(randFlag + ' **' + flags[randFlag].country.split('|').join(', ') + '**')
+  channels[msg.channel.id].pending = false
+}
+
+/**
  * Check the answer
  * @param msg message content
  * @param flag answer
+ * @param mode
  */
-function checkAnswer(msg, flag) {
-  return removeAccents(msg).replace(/-/g, ' ').toUpperCase()
-    === removeAccents(flag).replace(/-/g, ' ').toUpperCase()
+function checkAnswer(msg, flag, mode) {
+  if (mode === 'capital') {
+    flag = flag['capital']
+  } else if (mode === 'flag') {
+    flag = flag['country']
+  } else {
+    return
+  }
+
+  let b = false
+  for (const i of flag.split('|')) {
+    if (removeAccents(msg).replace(/-/g, ' ').replace(/\./g, '').toUpperCase().trim()
+        === removeAccents(i).replace(/-/g, ' ').replace(/\./g, '').toUpperCase().trim()) {
+      b = true
+      break
+    }
+  }
+  return b
 }
 
 /**
@@ -59,17 +101,24 @@ client.on('message', async (msg) => {
     if (channels[msg.channel.id].pending || msg.content.startsWith('!')) return
 
     // Stop the game
-    if ((command === 'flag' && args[0] === 'stop') || /.*(je(\s.*ne)?\s.*sais\s.*(plus|pas)|aucune\s.*idée).*/i.test(msg.content)) {
+    if ((command === config.command && args[0] === 'stop') || /.*(je(\s.*ne)?\s.*sais\s.*(plus|pas)|aucune\s.*idée).*/i.test(msg.content)) {
+      const tmpMode = channels[msg.channel.id].mode
       const tmpFlag = flags[channels[msg.channel.id].flag]
       delete channels[msg.channel.id]
-      await msg.reply(config.text.stop + ' ' + tmpFlag)
+
+      if (tmpMode === 'capital') {
+        await msg.reply(config.text.stop + ' **' + tmpFlag['capital'] + '**')
+      } else if (tmpMode === 'flag') {
+        await msg.reply(config.text.stop + ' **' + tmpFlag['country'] + '**')
+      }
 
     // Check if answer is correct
-    } else if (checkAnswer(msg.content, flags[channels[msg.channel.id].flag])) {
+    } else if (checkAnswer(msg.content, flags[channels[msg.channel.id].flag], channels[msg.channel.id].mode)) {
       // Check if loop mode
       if (channels[msg.channel.id].loop) {
         await msg.react('👍')
-        await sendQuestion(msg, true)
+        if (channels[msg.channel.id].mode === 'capital') await sendCapital(msg, true)
+        else if (channels[msg.channel.id].mode === 'flag') await sendFlag(msg, true)
       } else {
         delete channels[msg.channel.id] // Delete the current channel in object
         await msg.reply('Bravo!')
@@ -77,16 +126,20 @@ client.on('message', async (msg) => {
     } else await msg.react('👎')
 
   // Check for prefix + flag commands
-  } else if (command === 'flag') {
+  } else if (command === config.command && msg.content.startsWith(config.prefix)) {
     // Not loop mode
     if (!args.length) {
       await msg.channel.send(config.text.flagQuestion)
-      await sendQuestion(msg, false)
+      await sendFlag(msg, false)
 
     // Loop mode
     } else if (args[0] === 'loop') {
       await msg.channel.send(config.text.flagQuestion + ' ' + config.text.flagLoop)
-      await sendQuestion(msg, true)
+      await sendFlag(msg, true)
+
+    } else if (args[0] === 'capital' && args[1] === 'loop') {
+      await msg.channel.send(config.text.capitalQuestion + ' ' + config.text.flagLoop)
+      await sendCapital(msg, true)
 
     // Help
     } else if (args[0] === 'help') {
